868aa4563cc000dcf262769d58f402eb850f96c5
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-11-07 10:16:13 +0200

    added header and footer

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,20 +1,20 @@
-import '../scss/main.scss';
-import './page.scss'
+import "../scss/main.scss";
+import "./page.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded', 'page-index')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded", "page-index");
+});
 
 class Foo {
-  constructor () {
-    this.value = 'foobar'
+  constructor() {
+    this.value = "foobar";
   }
-  static instance () {
-    return new Foo()
+  static instance() {
+    return new Foo();
   }
-  getValue () {
-    return this.value
+  getValue() {
+    return this.value;
   }
 }
 
-console.log(Foo.instance().getValue())
+console.log(Foo.instance().getValue());
